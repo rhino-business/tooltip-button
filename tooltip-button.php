b977ebbd641ab47f491cdc80497bc40aa401f322
rhino <rhino@mailfence.com>
2025-03-09 19:47:56 +0100

    add

---
+++ tooltip-button.php
@@ -0,0 +1,32 @@
+<!--
+/**
+* Tooltip Button
+* UX Button / HTML elements
+*
+*
+* @ Author Jane Rhino <rhino@mailfence.com>
+* @ Github https://github.com/rhino-business/tooltip-button
+* @ Copyright 2023 rhino@mailfence.com
+* @ Link https://rhino.business
+* @ Readme https://github.com/rhino-business/tooltip-button/blob/main/README.md
+* @ License https://github.com/rhino-business/tooltip-button/blob/main/LICENSE
+* @ Released on: October, 27 2023
+*/
+-->
+
+<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <title>Tooltip Button</title>
+            <meta name="description" content="Adding a tooltip to a button">
+             <link rel="stylesheet" href = "styles/tooltip-button.css">  
+      </head>
+        <body>  
+          <div class = "container">
+	         <button tooltip = "Search your tickets" id = "tooltip_button">Search now</button>
+	      </div>
+	    </body>
+	 </html> 
+	          
